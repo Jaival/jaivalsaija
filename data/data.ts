2856b09bd2affcc62700f0d6fdc341ddf37f0790
Jaival <saijajaival@gmail.com>
2022-11-24 22:26:54 +0530

    Create data.ts
    User Data file

---
+++ data/data.ts
@@ -0,0 +1,93 @@
+// TODO: Add type definitions for data.ts, imgUrl's, Company link's, etc.
+const userData = {
+    githubUsername: "Jaival",
+    name: "Jaival Saija",
+    designation: "Cloud Engineer",
+    avatarUrl: "/avatar.png",
+    email: "saijajaival@gmail.com",
+    phone: "+91 8155060477",
+    address: "Ahmedabad, Gujarat, India",
+    projects: [
+        {
+            title: "Portfolio",
+            link: "https://tailwindmasterkit.com",
+            // imgUrl: "/tmk.jpg",
+        },
+        {
+            title: "Taskly",
+            link: "https://placeholdertech.in",
+            // imgUrl: "/placeholdertech.png",
+        },
+        {
+            title: "VS Code Flat Theme",
+            link: "https://manuarora.in",
+            // imgUrl: "/portfolio.png",
+        },
+    ],
+    about: {
+        title:
+            "I'm a software developer that loves building products and web applications that impact millions of lives",
+        description: [
+            `I've been interested in computer science since I was 15 years old, first is was because of the games and I dreamed of becoming a game developer or something related to computers. After few year of playing games and studying I got into a specialized bachelor's program in computer science for cloud computing.`,
+            `It was like a new world to open to exploration and [something] . As I was learning about cloud computing, how it works, why it works, how it came to existence; I was falling in love with it more and more.`,
+            `During my bachelor's program, I worked with many technologies like AWS, Azure, Google Cloud, Firebase, Flutter, Android and many more.`,
+            `The most exposure I got to cloud computing was through the projects in the last year of my college. I was a part of a team and I was responsible for the cloud operations such as creating the infrastructure, maintaining it, CI/CD, as well as selecting a DB for the application that was being developed.`,
+        ],
+        currentProject: "Taskly",
+        //   currentProjectUrl: "https://tailwindmasterkit.com",
+    },
+    experience: [
+        {
+            title: "Junior DevOps Engineer",
+            company: "Maven Infosoft Pvt. Ltd.",
+            year: "2022",
+            // companyLink: "https://mroads.com",
+            desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+        },
+        {
+            title: "Graduation",
+            company: "Ganpat University| Institute of Computer Technology",
+            year: "2021",
+            // companyLink: "https://upes.co.in",
+            desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+        },
+        {
+            title: "Industry Internship ",
+            company: "Apexa Information System Pvt. Ltd.",
+            year: "2019",
+            // companyLink: "https://tailwindmasterkit.com",
+            desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+        },
+        {
+            title: "In-House Internship",
+            company: "Ganpat University| Institute of Computer Technology",
+            year: "2019",
+            // companyLink: "https://algochurn.com",
+            desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+        },
+        {
+            title: "Social Internship",
+            company: "Karma Foundation",
+            year: "2018",
+            // companyLink: "https://intel.com",
+            desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+        },
+        {
+            title: "High School",
+            company: "Florescent public school, Ahmedabad.",
+            year: "2017",
+            // companyLink: "https://upes.co.in",
+            desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+        },
+    ],
+    resumeUrl:
+        "https://drive.google.com/file/d/10ASoWhGBGDJtTCOhwJue_jgM64z-eNUY/view?usp=sharing",
+    socialLinks: {
+        instagram: "https://www.instagram.com/jaivalsaija/",
+        twitter: "https://twitter.com/Jaival469",
+        linkedin: "https://www.linkedin.com/in/jaival-saija-113452152/",
+        github: "https://github.com/Jaival",
+    },
+};
+
+export default userData;

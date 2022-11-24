eb20ec08dc23d5e6339c9ed02ed926db60d2c3ee
Jaival <saijajaival@gmail.com>
2022-11-24 22:32:48 +0530

    Contact Me Page Created

---
+++ pages/contactme/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContactMe from "../../components/contactme";
+import ContainerMain from "../../components/mainContainer";
+
+export default function contactMe() {
+    return (
+        <ContainerMain title="Contact - Jaival Saija">
+            <ContactMe />
+        </ContainerMain>
+    );
+}
